37fba26768619250723167ed087f2163dd51a38c
Vinogradova Darina <48646373+VinogradovaD@users.noreply.github.com>
2021-11-02 19:56:22 +0300

    Update task4.js

--- Lesson 1/task4.js
+++ Lesson 1/task4.js
@@ -1,3 +1,16 @@
+/* 
+    Задание 4:
+    
+    Перед вами код:
+    const randomNumber = Math.floor(Math.random() * 100);
+
+    Этот код при каждом обновлении страницы генерирует случайное число и записывает его в переменную randomNumber.
+
+    Напишите условную конструкцию, со следующими данными:
+    - если randomNumber меньше 20, то выведите в консоль сообщение : "randomNumber меньше 20"
+    - если randomNumber больше 50, то выведите в консоль сообщение : "randomNumber больше 50"
+    - если ни один из вариантов не совпал, то выведите в консоль сообщение : "randomNumber больше 20, и меньше 50"
+*/
 const randomNumber = Math.floor(Math.random() * 100);
 if (randomNumber < 20)
     console.log("randomNumber меньше 20");
