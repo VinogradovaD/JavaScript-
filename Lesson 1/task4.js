4a8140565854ce0bc788bcae1084f9dc8f3340cb
Vinogradova Darina <48646373+VinogradovaD@users.noreply.github.com>
2021-11-03 13:35:30 +0300

    Update task4.js

--- Lesson 1/task4.js
+++ Lesson 1/task4.js
@@ -12,9 +12,10 @@
     - если ни один из вариантов не совпал, то выведите в консоль сообщение : "randomNumber больше 20, и меньше 50"
 */
 const randomNumber = Math.floor(Math.random() * 100);
-if (randomNumber < 20)
+if (randomNumber < 20) {
     console.log("randomNumber меньше 20");
-else if (randomNumber > 50);
-    console.log("randomNumber больше 50");
-еlse 
+} else if (randomNumber > 50) {
+    console.log("randomNumber больше 50")
+} еlse {
     console.log("randomNumber больше 20, и меньше 50");
+}
