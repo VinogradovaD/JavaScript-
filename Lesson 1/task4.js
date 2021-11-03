7e163b3d23d463468cb923c63e2fadb0be9b5d3d
Vinogradova Darina <48646373+VinogradovaD@users.noreply.github.com>
2021-11-03 13:35:55 +0300

    Update task4.js

--- Lesson 1/task4.js
+++ Lesson 1/task4.js
@@ -16,6 +16,6 @@ if (randomNumber < 20) {
     console.log("randomNumber меньше 20");
 } else if (randomNumber > 50) {
     console.log("randomNumber больше 50")
-} еlse {
+} else {
     console.log("randomNumber больше 20, и меньше 50");
 }
