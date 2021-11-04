6fe5e5b98d9c5fb1778ef1741bc469c7757998f8
Vinogradova Darina <48646373+VinogradovaD@users.noreply.github.com>
2021-11-04 23:18:45 +0300

    Add files via upload

---
+++ Lesson 2/task2.js
@@ -0,0 +1,18 @@
+/* 
+    Задание 2:
+
+    Необходимо создать объект в котором вы опишите самого себя в формате : "свойство: значение"
+
+    Список свойств:
+
+    - Имя(string)
+    - Фамилия(string)
+    - Возраст(number)
+    - Есть ли домашние животные(boolean)
+*/
+const aboutMe = {
+    firstName: 'Darina',
+    lastName: 'Vinogradova',
+    age: 22,
+    haveAnimal: true
+};
