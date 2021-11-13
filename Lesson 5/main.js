aaf47c1a33ce9ca70844945a7f904d9bc44c6d63
Vinogradova Darina <48646373+VinogradovaD@users.noreply.github.com>
2021-11-13 20:41:15 +0300

    Add files via upload

---
+++ Lesson 5/main.js
@@ -0,0 +1,186 @@
+const option1= document.querySelector('.option1'),
+      option2= document.querySelector('.option2'),
+      option3= document.querySelector('.option3'),
+      option4= document.querySelector('.option4');
+
+const optionElements = document.querySelectorAll('.option');
+
+const question = document.getElementById('question');
+
+const numberOfQuestion = document.getElementById('number-of-question'),
+      numberOfAllQuestions = document.getElementById('number-of-all-questions');
+
+let indexOfQuestion, 
+    indexOfPage = 0;
+
+const answersTracker = document.getElementById('answers-tracker');
+
+const btnNext = document.getElementById('btn-next');
+
+let score = 0;
+
+const correctAnswer = document.getElementById('correct-answer'),
+      numberOfAllQuestions2 = document.getElementById('number-of-all-questions-2'),
+      btnTryAgain = document.getElementById('btn-try-again');
+
+const quizOverModal = document.querySelector('.quiz-over-modal');
+
+const result = document.getElementById('result');
+
+const questions = [
+    {
+        question: 'Что не умеет делать JavaScript?',
+        options: [
+            'Добавлять новый HTML-код на страницу',
+            'Модифицировать стили',
+            'Писать код за программиста',
+            'Реагировать на действия пользователя',
+        ],
+        rightAnswer: 2
+    },
+    {
+        question: 'Что из перечисленного не является фреймворком JavaScript?',
+        options: [
+            'Vue',
+            'Django',
+            'Angular',
+            'React',
+        ],
+        rightAnswer: 1
+    },
+    {
+        question: 'Какой лучший способ начать изучать JavaScript?',
+        options: [
+            'Купить пособие по JavaScript',
+            'Почитать статьи в интернете',
+            'Найти друга-программиста',
+            'Записаться на бесплатный курс от WayUp!',
+        ],
+        rightAnswer: 3
+    }
+];
+
+numberOfAllQuestions.innerHTML = questions.length;
+
+const load = () => {
+    question.innerHTML = questions[indexOfQuestion].question;
+
+    option1.innerHTML = questions[indexOfQuestion].options[0];
+    option2.innerHTML = questions[indexOfQuestion].options[1];
+    option3.innerHTML = questions[indexOfQuestion].options[2];
+    option4.innerHTML = questions[indexOfQuestion].options[3];
+
+    numberOfQuestion.innerHTML = indexOfPage + 1;
+    indexOfPage++;
+};
+
+let completedAnswers = [];
+
+const randomQuestion = () => {
+    let randomNumber = Math.floor(Math.random() * questions.length);
+    let hitDuplicate = false;
+
+    if(indexOfPage == questions.length) {
+        quizOver();
+    } else {
+        if(completedAnswers.length > 0){
+            completedAnswers.forEach(item => {
+                if(item == randomNumber){
+                    hitDuplicate = true;
+                }
+            });
+            if(hitDuplicate){
+                randomQuestion();
+            } else {
+                indexOfQuestion = randomNumber;
+                load();
+            }
+        }
+        if(completedAnswers.length == 0) {
+            indexOfQuestion = randomNumber;
+            load();
+        }   
+    }
+    console.log(indexOfQuestion);
+    completedAnswers.push(indexOfQuestion);  //добавляем элемент в массив
+};
+
+const checkAnswer = el => {
+    if(el.target.dataset.id == questions[indexOfQuestion].rightAnswer) {
+        el.target.classList.add('correct');
+        updateAnswerTracker('correct');
+        score++;
+    } else {
+        el.target.classList.add('wrong');
+        updateAnswerTracker('wrong');
+    }
+    disabledOptions();
+};
+
+const disabledOptions = () => {
+    optionElements.forEach(item => {
+        item.classList.add('disabled');
+        if(item.dataset.id == questions[indexOfQuestion].rightAnswer) {
+            item.classList.add('correct');
+        }
+    });
+};
+
+const enabledOptions = () => {
+    optionElements.forEach(item => {
+        item.classList.remove('disabled', 'correct', 'wrong');
+        });
+};
+
+const updateAnswerTracker = status => {
+    answersTracker.children[indexOfPage - 1].classList.add(`${status}`);
+};
+
+const validate = () => {
+    if(!optionElements[0].classList.contains('disabled')) {
+        alert('Вам нужно выбрать один ответ.')
+    } else {
+        randomQuestion();
+        enabledOptions();
+    }
+
+};
+
+const answerTracker = () => {
+    questions.forEach(() => {
+        const div = document.createElement('div');
+        answersTracker.appendChild(div);
+    });
+};
+
+for(option of optionElements){
+    option.addEventListener('click', e => checkAnswer(e));
+};
+
+const quizOver = () => {
+    if(score == 0){
+        result.innerHTML = 'Не отчаивайся! Попробуй снова!'
+    } else {
+        result.innerHTML = 'Хороший результат!'
+    }
+    quizOverModal.classList.add('active');
+    correctAnswer.innerHTML = score;
+    numberOfAllQuestions2.innerHTML = questions.length;
+};
+
+const tryAgain = () => {
+    window.location.reload(); //перезагружаем страницу.
+};
+
+btnTryAgain.addEventListener('click', tryAgain);
+btnNext.addEventListener('click', () => {
+    validate();
+});
+
+window.addEventListener('load', () => { 
+    randomQuestion();
+    answerTracker();
+}); //если страница прогрузилась, то вызывается функции.
+
+
+
